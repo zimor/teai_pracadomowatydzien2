5a03833e2807b8dbc8729ec8e443ff99d5f7d24b
Michał Zimorodzki <zimorek@gmail.com>
2020-03-01 17:16:21 +0100

    Initial commit

---
+++ src/main/java/pl/zimor/sbhw2/Sbhw2Application.java
@@ -0,0 +1,13 @@
+package pl.zimor.sbhw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbhw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbhw2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zimor/sbhw2/Sbhw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zimor.sbhw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbhw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
